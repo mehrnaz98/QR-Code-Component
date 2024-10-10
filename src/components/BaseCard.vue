8cde7d9d4571812bfc77e62b06ca265aa4d955c2
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-10-10 21:52:07 +0330

    Update image div

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,7 +1,11 @@
 <template>
-  <section>
-    <div>
-      <img src="" alt="" />
+  <section class="w-80 bg-white border border-none rounded-xl shadow-xl">
+    <div class="p-4">
+      <img
+        src="../assets/image/image-qr-code.png"
+        class="border border-none rounded-xl"
+        alt="QR Code"
+      />
     </div>
 
     <div>
